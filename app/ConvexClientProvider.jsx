c7bed1fe211ab264ddd17a596bd819d23221f401
sagar3595 <sagar47kumar13@gmail.com>
2026-02-27 00:52:20 +0530

    removed space

--- app/ConvexClientProvider.jsx
+++ app/ConvexClientProvider.jsx
@@ -4,7 +4,7 @@ import { useAuth } from "@clerk/nextjs";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 import {ConvexProviderWithClerk} from "convex/react-clerk";
 
-const convex = new ConvexReactClient(process.env. NEXT_PUBLIC_CONVEX_URL);
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
 
 export function ConvexClientProvider({ children }) {
   return <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
